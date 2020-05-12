31a2481c939c54e32c3f44154dd6bd2501f4742d
quocanh0212 <anh.nq0212@gmail.com>
2020-05-12 16:59:36 +0700

    Imitial commit

---
+++ src/screens/Personal/UserProfile.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+
+function UserProfile ({navigation}) {
+        return (
+            <View>
+                <Text>UserProfile</Text>
+            </View>
+        );
+
+}
+
+export default UserProfile;
+
+const styles = StyleSheet.create({
+
+})
