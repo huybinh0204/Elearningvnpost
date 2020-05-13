ba73eba0aa01711d50a9af0bba0c60f54e86ce95
KhanhTT <tuankhanhtran45@gmail.com>
2020-05-13 08:36:44 +0700

    config assets app

---
+++ src/assets/String.js
@@ -0,0 +1,9 @@
+const VN_string = {
+  app_name: 'E Learning VNPost',
+};
+const EN_string = {};
+
+export default {
+  VN_string,
+  EN_string,
+};
---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import Colors from './Colors';
+import Fonts from './Fonts';
+import Metrics from './Metrics';
+import Images from './Images';
+import String from './String';
+
+export {Colors, Fonts, Images, Metrics, String};
